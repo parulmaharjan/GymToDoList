866f0fd3e74687929be305f8fd33c71b94929d59
parul <parulmhrzn@gmail.com>
2023-10-03 22:10:52 +0545

    subscription file update(complete)

--- subscription/subscribed.php
+++ subscription/subscribed.php
@@ -1,39 +1,32 @@
 <?php
-// Assuming you've already started a session
 session_start();
+
+// Connect to the MySQL database
 require_once("../database/config.php");
-
-// Retrieve the username from the session
-$username = $_SESSION['username'];
-
-// Retrieve the selected package ID from the form submission
-$selectedPackageID = $_POST['packageId']; // Make sure this matches the data attribute in your JavaScript code
-
-// Perform database connection (you should configure this)
-$mysqli = new mysqli(host, user, password, db);
-
+$conn = new mysqli(host, user, password, db);
 // Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// Insert the user's subscription into the usersubscription table
-$query = "INSERT INTO usersubscriptions (username, package_Id) VALUES (?, ?)"; // Assuming your table name is usersub
-
-$stmt = $mysqli->prepare($query);
-
-if ($stmt) {
-    $stmt->bind_param("si", $username, $selectedPackageID);
-    $stmt->execute();
-    $stmt->close();
-    
-    // Subscription successfully recorded
-    echo "Subscription successful!";
+// Get data from the HTML form
+if (isset($_POST['packageId'])) {
+    $packageId = $_POST['packageId'];
 } else {
-    // Error handling for the database query
-    echo "Error: " . $mysqli->error;
+    echo "Error: packageId not defined";
+    exit;
 }
 
-// Close the database connection
-$mysqli->close();
+$username = $_SESSION['username']; // Make sure the user is logged in and you have the session variable
+
+// Insert data into the database
+$sql = "INSERT INTO usersubscription (packageId, username, subscription_date) VALUES ('$packageId', '$username', NOW())";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: ../login/login.html");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
