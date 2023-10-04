44c8cd6181956988c86cad07e83faf8e1c1332dc
parul <parulmhrzn@gmail.com>
2023-10-04 22:21:09 +0545

    register file updates solved db problem

--- register/register.php
+++ register/register.php
@@ -1,11 +1,10 @@
 <?php
-// Include your database configuration file
+    session_start();
+
 require_once("../database/config.php");
 
-// Establish a database connection
-$mysqli = new mysqli(host, user, password, db);
 
-// Check for connection errors
+$mysqli = new mysqli(host, user, password, db);
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
@@ -16,8 +15,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastName = $_POST["lastName"];
     $email = $_POST["email"];
     $phoneNumber = $_POST["phoneNumber"];
-    $userName = $_POST["userName"];
+    $username = $_POST["username"];
     $password = $_POST["password"];
+ 
+
 
     // Server-side validation
     $errors = [];
@@ -53,9 +54,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Check if username already exists in the database
-    $check_query = "SELECT * FROM registereduser  WHERE userName = ?";
+    $check_query = "SELECT * FROM registereduser  WHERE username = ?";
     $check_stmt = $mysqli->prepare($check_query);
-    $check_stmt->bind_param("s", $userName);
+    $check_stmt->bind_param("s", $username);
     $check_stmt->execute();
     $result = $check_stmt->get_result();
     
@@ -101,12 +102,24 @@ if ($check_stmt->num_rows > 0) {
         // Insert data into the database
         $insert_query = "INSERT INTO registereduser (first_Name, middle_Name, last_Name, email, phone_Number, username, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
         $stmt = $mysqli->prepare($insert_query);
-        $stmt->bind_param("sssssss", $firstName, $middleName, $lastName, $email, $phoneNumber, $userName, $passwordHash);
+        $stmt->bind_param("sssssss", $firstName, $middleName, $lastName, $email, $phoneNumber, $username, $passwordHash);
+        $_SESSION['username'] = $username;
+
+// Check if the 'username' session variable is set
+if (isset($_SESSION['username'])) {
+    // Username is stored in the session
+    $username = $_SESSION['username'];
+    echo "Username is: " . $username;
+} else {
+    // Username is not stored in the session
+    echo "Username is not set in the session.";
+}
+
 
         if ($stmt->execute()) { 
             // Registration was successful, redirect to subscription.html
             header("Location: ../subscription/subscribed.html");
-            exit; // Make sure to exit after redirection
+            exit(); // Make sure to exit after redirection
         } else {
             echo "Error: " . $stmt->error;
         }
