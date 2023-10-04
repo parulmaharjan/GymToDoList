7a8cee87d3c4973fd638db12ab74e2e094ce7ba7
parul <parulmhrzn@gmail.com>
2023-10-04 22:23:44 +0545

    exercise fetched from db

--- mainPage/main.php
+++ mainPage/main.php
@@ -1,28 +1,31 @@
 <?php
-// Establish a database connection (you should have database credentials here)
+session_start(); 
 require_once("../database/config.php");
-
 $conn = new mysqli(host, user, password, db);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Fetch data from the "packages" table
-$sql = "SELECT  packageName FROM packages";
-$result = $conn->query($sql);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$selected_day = $_POST['selected_day'];
+$username = $_SESSION['username'];
+if (isset($_POST['selected_day']) && isset($_SESSION['username'])) {
+    $selected_day = $_POST['selected_day'];
+    $username = $_SESSION['username'];
+} else {
+    echo "not set" ;
 }
+$sql = "SELECT exercise
+        FROM exercisedetails, usersubscription
+        WHERE exercisedetails.packageId = usersubscription.packageId
+        AND day = '$selected_day'
+        AND username = '$username';";
 
-$sql = "SELECT packageID FROM packages";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo '<li><label><input type="checkbox" name="packages[]" value="' . $row["packageID"] . '">' . $row["packageID"] . '</label></li>';
-    }
-}
-
+$result = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($result)) { 
+    header(" main.html");
+            echo '<label><input type="checkbox" name="exercise" value="' . $row['exercise'] . '"> ' . $row['exercise'] . '</label><br>';
+ } 
 $conn->close();
+?> 
+
+     
+   
