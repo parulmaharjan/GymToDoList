e2474c21e7cb030b57c944a9fe9cc8cc31f61117
parul <parulmhrzn@gmail.com>
2023-10-06 21:32:20 +0545

    main file updated

--- mainPage/main.js
+++ mainPage/main.js
@@ -1,20 +1,20 @@
- 
-  document.getElementById("generate").addEventListener("click", function () {
-    // Show the exercise container
-    document.querySelector(".exercise-container").style.display = "block";
-  });
-  
-  document.querySelector(".exercise-container button").addEventListener("click", function () {
-    // Get data from the checkboxes
-    const checkboxes = document.querySelectorAll('input[name="exercise"]:checked');
-    const totalExercise = checkboxes.length;
-    const selectedDay = document.getElementById("day-select").value;
-  
-    // Set the values of the hidden input fields
-    document.querySelector("input[name='selected_day']").value = selectedDay;
-    document.querySelector("input[name='total_exercise']").value = totalExercise;
-  
-    // Submit the form
-    document.getElementById("gym-form").submit();
-  });
-  
+document.getElementById("generate").addEventListener("click", function () {
+  // Show the exercise container when the "Generate" button is clicked
+  document.querySelector(".exercise-container").style.display = "block";
+  // Generate exercise checkboxes here (you may use AJAX for this)
+});
+
+document.getElementById("done-for-today").addEventListener("click", function () {
+  // Get data from the checkboxes
+  const checkboxes = document.querySelectorAll('input[name="exercise"]:checked');
+  const allTotal = document.querySelectorAll('input[name="exercise"]')
+  const totalExercise = allTotal.length;
+  const selectedDay = document.getElementById("day-select").value;
+
+  // Set the values of the hidden input fields
+  document.getElementById("selected_day").value = selectedDay;
+  document.getElementById("total_exercise").value = totalExercise;
+
+  // Submit the form
+  document.getElementById("gym-form").submit();
+});
